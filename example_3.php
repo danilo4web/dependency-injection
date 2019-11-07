ec0da126dfaae38eae98f07a4d6305236df56c52
Danilo Pereira <danilo4web@gmail.com>
2019-11-06 21:14:17 -0300

    Injection of two databases using interface

--- example_3.php
+++ example_3.php
@@ -18,7 +18,24 @@ class PdoDriver implements DatabaseDriver
 
     public function connect()
     {
-        $pdo = new \PdoDriver($this->config['dns'], $this->config['user'], $this->config['passwd']);
+        $pdo = new \PDO($this->config['dns'], $this->config['user'], $this->config['passwd']);
+
+        echo "PDO connected" . PHP_EOL;
+    }
+}
+
+class MongoDbDriver implements DatabaseDriver
+{
+    public function configure(array $config)
+    {
+        $this->config = $config;
+    }
+
+    public function connect()
+    {
+        $mongo = new \MongoClient($this->config['server']);
+
+        echo "MongoDb connected" . PHP_EOL;
     }
 }
 
@@ -36,10 +53,17 @@ class Database
 $ioc = [];
 $ioc['db'] = function () {
     $pdo_driver = new PdoDriver();
-    $pdo_driver->configure(['dns' => '0.0.0.0:3307', 'user' => 'symfony', 'passwd' => 'symfony']);
+    $pdo_driver->configure(['dns' => 'mysql:host=0.0.0.0:3307;dbname=symfony', 'user' => 'symfony', 'passwd' => 'symfony']);
 
     return new Database($pdo_driver);
 };
 
-$ioc['db']();
+$ioc['db_mongo'] = function () {
+    $mongo_driver = new MongoDbDriver();
+    $mongo_driver->configure(['server' => 'mongodb://localhost:27017']);
 
+    return new Database($mongo_driver);
+};
+
+$ioc['db']();
+$ioc['db_mongo']();
