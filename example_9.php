5be57e4506ff3759d85b74e26311bec3c27483ca
Danilo Pereira <danilo4web@gmail.com>
2019-11-07 14:33:00 -0300

    ServiceManager options param

--- example_9.php
+++ example_9.php
@@ -1,51 +1,14 @@
 <?php
 
-require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 use Zend\ServiceManager\ServiceManager;
-use Interop\Container\ContainerInterface;
-use Zend\ServiceManager\Factory\FactoryInterface;
-
-class TestAdapter
-{
-    public function __construct()
-    {
-        var_dump(TestAdapter::class.'::__construct()');
-    }
-
-    public function runTest($message)
-    {
-        var_dump($message);
-    }
-}
-
-class Tester
-{
-    public function __construct(TestAdapter $adapter)
-    {
-        $adapter->runTest('Rodou um test');
-    }
-}
-
-class TesterFactory implements FactoryInterface
-{
-    public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
-    {
-        return new Tester($container->get('ta'));
-    }
-}
 
 $serviceManager = new ServiceManager([
-   'factories' => [
-        'ta' => function(ContainerInterface $container) {
-            return new TestAdapter();
-        },
-       'tester' => TesterFactory::class
-   ]
+    'services' => [],
+    'factories' => [],
+    'abstract_factories' => [],
+    'delegators' => [],
+    'shared' => [],
+    'shared_by_default' => [],
 ]);
-
-#build - create new instance
-$tester_1 = $serviceManager->build('tester');
-$tester_2 = $serviceManager->build('tester');
-
-var_dump($tester_1, $tester_2);
